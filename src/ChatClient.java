986befe12cf47df277550ea812ea12810103cad6
tangzhengyue <ty54d1@163.com>
2013-09-07 21:48:21 +0800

    Chat server

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,8 +1,5 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class ChatClient extends Frame{
 	TextField txText = new TextField();
